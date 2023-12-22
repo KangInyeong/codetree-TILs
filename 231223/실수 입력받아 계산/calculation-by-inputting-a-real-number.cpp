cac8d27128d5ddb07cd0c04285133d17e27a75f1
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2023-12-23 01:34:13 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231223/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a;
+    cin >> b;
+    
+    cout << fixed;
+    cout.precision(2);
+    cout << a + b;
+    return 0;
+}
