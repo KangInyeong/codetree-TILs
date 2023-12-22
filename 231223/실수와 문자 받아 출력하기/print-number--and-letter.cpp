ce194a548461a540cb08f521a61003c608a4b9ce
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2023-12-23 03:02:45 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231223/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string c;
+    float a, b;
+
+    cin >> c >> a >> b;
+    cout << fixed;
+    cout.precision(2);
+
+    cout << c << '\n' << a << '\n' << b ; 
+    return 0;
+}
