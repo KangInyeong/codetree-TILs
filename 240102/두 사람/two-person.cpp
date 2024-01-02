c437fda5c74fda8420ad3aa258bc1f15caaf283e
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-01-02 23:40:15 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/두 사람/two-person.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int aa, ba;
+    char as, bs;
+
+    cin >> aa >> as >> ba >> bs;
+    if((aa>=19 && as=='M') || (ba>=19&&bs=='M')){
+        cout << 1;
+    }else{
+        cout << 0;
+    }
+    return 0;
+}
