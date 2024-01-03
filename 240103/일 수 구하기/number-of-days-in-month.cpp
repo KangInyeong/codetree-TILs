674f071986fad8d4e28ab1a1da1d171b77299418
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-01-03 17:25:06 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240103/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y;
+    cin >> y;
+
+    if(y == 2){
+        cout << 28;
+        return 0;
+    }
+
+    if((y%2==0 && y<=7) || (y%2==1 && y>=7)){
+        cout << 30;
+    }else{
+        cout << 31;
+    }
+    return 0;
+}
