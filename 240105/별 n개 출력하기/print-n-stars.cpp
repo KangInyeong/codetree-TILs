4f560f6850b91e1e7dab092c53996acb0707ff38
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-01-05 19:24:37 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240105/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    int i=1;
+    while(i<n+1){
+        cout << "*" << '\n';
+        i++;
+    }
+    return 0;
+}
