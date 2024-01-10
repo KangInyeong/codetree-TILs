aec20663ef1eaec09f0afa1d0b9bee5f8f60b5b2
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-01-11 04:17:46 +0900

    Title: 1이 3개 이상 있는 위치, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 240111/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -1,45 +1,48 @@
+// 해설
 #include <iostream>
+#include <string>
+
+#define MAX_N 100
+#define DIR_NUM 4
+
 using namespace std;
 
 int n;
-int arr[100][100];
+int arr[MAX_N][MAX_N];
 
-bool InRange(int x, int y){
-    return (0 <= x && x < n && 0 <= y && y < n);
+int dx[DIR_NUM] = {0, 1,  0, -1};
+int dy[DIR_NUM] = {1, 0, -1,  0};
+
+bool InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {-1, 1, 0, 0};
-
-int IsOne(int x, int y){
+int AdjacentCnt(int x, int y) {
     int cnt = 0;
-
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(InRange(ny, nx) && arr[ny][nx] == 1) cnt++;
+    for(int i = 0; i < DIR_NUM; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if(InRange(nx, ny) && arr[nx][ny] == 1)
+            cnt++;
     }
-
+    
     return cnt;
 }
 
 int main() {
-    
+    // 입력
     cin >> n;
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+    
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
             cin >> arr[i][j];
-        }
-    }
-
-    int cnt = 0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(IsOne(j, i) >= 3) cnt++;
-        }
-    }
-
-    cout << cnt;
+    
+    // 각 칸을 탐색합니다.
+    int ans = 0;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(AdjacentCnt(i, j) >= 3)
+                ans++;
+    
+    cout << ans;
     return 0;
 }
