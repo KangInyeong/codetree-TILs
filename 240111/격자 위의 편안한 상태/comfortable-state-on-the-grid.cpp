faa9bac8551cddb6cbc0dadf4575b85b0347e510
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-01-11 22:27:46 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int arr[101][101] = {0, };
+int n, m;
+int dy[4] = {-1,1,0,0};
+int dx[4] = {0,0,-1,1};
+
+int check(int y, int x){
+    int cnt = 0;
+
+    for(int i=0; i<4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny<0&&nx<0&&ny>=n&&n>=n) continue;
+        if(arr[ny][nx] == 1)cnt++;
+    }
+    if(cnt >= 3) return 1;
+    else return 0;
+}
+
+int main() {
+    
+    cin >> n >> m;
+    for(int i=0; i<m; i++){
+        int r, c;
+        cin >> r >> c;
+        arr[r][c] = 1;
+        cout << check(r,c) << endl;
+    }
+    return 0;
+}
