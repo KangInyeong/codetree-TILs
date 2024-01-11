312255c4183fc47943af81e7e5bfc6fd76ef2acc
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-01-12 03:36:15 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240112/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -4,37 +4,36 @@ using namespace std;
 int n, m;
 int grid[101][101];
 int visited[101][101];
-int dy[2] = {1, 0};
-int dx[2] = {0, 1};
+int dy[2] = { 1, 0 };
+int dx[2] = { 0, 1 };
 
-void dfs(int y, int x){
-    for(int i=0; i<2; i++){
+void dfs(int y, int x) {
+    for (int i = 0; i < 2; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
 
-        if(visited[ny][nx]!=0) continue;
-        if(ny<0 && nx<0 && ny>=n && nx>=m) continue;
-        if(grid[ny][nx] == 0) continue;
+        if (ny < 0 && nx < 0 && ny >= n && nx >= m) continue;
+        if (visited[ny][nx] != 0) continue;
+        if (grid[ny][nx] == 0) continue;
 
-        visited[ny][nx] == 1;
+        visited[ny][nx] = 1;
         dfs(ny, nx);
     }
 }
 
 int main() {
-    
+
     cin >> n >> m;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> grid[i][j];
         }
     }
 
     visited[0][0] = 1;
-    dfs(0,0);
-
-    if(visited[n-1][m-1] == 1) cout << 1;
-    else cout << 0;
+    dfs(0, 0);
+    
+    cout << visited[n - 1][m - 1];
 
     return 0;
 }
