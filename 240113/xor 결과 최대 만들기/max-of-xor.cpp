f43eaeaff51c7c13f7ed591b395a2a8bee2c1fdf
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-01-13 04:44:05 +0900

    Title: xor 결과 최대 만들기, Time: 1074ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240113/xor 결과 최대 만들기/max-of-xor.cpp
+++ 240113/xor 결과 최대 만들기/max-of-xor.cpp
@@ -9,16 +9,16 @@ int answer = -1;
 
 void choose(int num){
     if(num == m+1){
-        int tmp = v[0];
-        for(int i=1; i<v.size(); i++){
-            tmp = tmp ^ v[i];
+        int tmp = 0;
+        for(int i=0; i<v.size(); i++){
+            tmp ^= v[i];
         }
         if(tmp > answer) answer = tmp;
         return;
     }
 
     for(int i=0; i<n; i++){
-        v.push_back(i);
+        v.push_back(arr[i]);
         choose(num+1);
         v.pop_back();
     }
