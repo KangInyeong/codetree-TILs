3916ea023fe2c566fe73e84497c2048362ffcd72
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-02-02 00:00:08 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240202/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int a, b, c;
+
+bool checkNum(int i){
+    if(c % i) return true;
+    else return false;
+}
+int main() {
+    
+    cin >> a >> b >> c;
+
+    bool flag = false;
+    for(int i=a; i<=b; i++){
+        if(checkNum(i)){
+            flag = true;
+            break;
+        }
+        
+    }
+
+    if(flag) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
