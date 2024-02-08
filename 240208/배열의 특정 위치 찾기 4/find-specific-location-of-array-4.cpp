abff44a6840626bfef7f4ac21e48d54e8069fd7f
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-02-08 23:36:42 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[10];
+    int cnt = 0;
+    int sum = 0;
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+        if(arr[i]%2 == 0 && arr[i]!=0){
+            cnt++;
+            sum += arr[i];
+        }
+        if(arr[i] == 0){
+           break; 
+        }
+    }
+
+    cout << cnt << ' ' << sum;
+    return 0;
+}
