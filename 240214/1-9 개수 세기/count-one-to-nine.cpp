492237e17c4c70bb67541a7b93d3e49c05c6795f
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-02-14 23:54:26 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240214/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    int arr[10] = {0, };
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        arr[a]++;
+    }
+
+    for(int i=1; i<=9; i++){
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
