eadeb26ac768885b27380b877639282ed1b8116f
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-02-23 01:06:51 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240223/가장 짧은 문자열/shortest-string.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    if(s1.length() > s2.length()){
+        if(s1.length()>s3.length()){
+            if(s2.length() > s3.length()){
+                cout << s1.length() - s3.length();
+            }else{
+                cout << s1.length() - s2.length();
+            }
+        }else{
+            cout << s3.length() - s2.length();
+        }
+    }else{
+        if(s1.length()>s3.length()){
+            cout << s2.length() - s3.length();
+        }else{
+            if(s2.length() > s3.length()){
+                cout << s2.length() - s1.length();
+            }else{
+                cout << s3.length() - s1.length();
+            }
+        }
+    }
+
+    return 0;
+}
