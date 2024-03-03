853c67c60b72f55e027f741eaa97fc6addd44632
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-03-03 18:33:57 +0900

    Title: 문자열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240303/문자열 놀이/play-with-string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string s;
+    int q;
+    cin >> s >> q;
+
+    for(int i=0; i<q; i++){
+        int com;
+        cin >> com;
+        if(com == 1){
+            int a, b;
+            cin >> a >> b;
+            char tmp = s[a];
+            s[a] = s[b];
+            s[b] = tmp;
+        }else{
+            char a, b;
+            cin >> a >> b;
+            for(int i=0; i<s.length(); i++){
+                if(s[i] == a){
+                    s[i] = b;
+                }
+            }
+        }
+    }
+
+    cout << s;
+
+    return 0;
+}
