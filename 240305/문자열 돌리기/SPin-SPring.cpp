b266124080ac62273b0f8051da732bfe0d972aab
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-03-05 02:23:49 +0900

    Title: 문자열 돌리기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240305/문자열 돌리기/SPin-SPring.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string str;
+    cin >> str;
+    int l = str.length();
+    cout << str << endl;
+    for(int i=0; i<l; i++){
+        str = str.substr(len - 1, 1) + str.substr(0, len - 1);
+		cout << str << endl;
+    }
+
+    return 0;
+}
