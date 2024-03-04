ca93b2bb8a36e4613ee4c42df32740f7576aba6a
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-03-05 02:24:10 +0900

    Title: 문자열 돌리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/문자열 돌리기/SPin-SPring.cpp
+++ 240305/문자열 돌리기/SPin-SPring.cpp
@@ -9,7 +9,7 @@ int main() {
     int l = str.length();
     cout << str << endl;
     for(int i=0; i<l; i++){
-        str = str.substr(len - 1, 1) + str.substr(0, len - 1);
+        str = str.substr(l - 1, 1) + str.substr(0, l - 1);
 		cout << str << endl;
     }
 
