5e5967a9f21ec6ae60f4d35fe60f100d26d310b2
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-03-09 23:58:17 +0900

    Title: 소문자와 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240309/소문자와 숫자/letter-and-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string str;
+    cin >> str;
+    for(int i=0; i<str.length(); i++){
+        if(isalpha(str[i])){
+            if(str[i]>='a'&&str[i]<='z') cout << str[i];
+            else cout << (char)(str[i]-'A'+'a');
+        }else{
+            if(str[i]>='1' && str[i]<='9') cout << str[i];
+        }
+    }
+    return 0;
+}
