9a2e8fecb60b63626716fb3e0a9216721ad98427
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-03-14 23:42:06 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240314/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string a, b;
+    cin >> a >> b;
+
+    int n=1;
+    bool flag = false;
+    for(int i=0; i<a.length(); i++){
+        string newstr = a.substr(n, a.length()) + a.substr(0,n);
+        if(newstr == b) {
+            flag = true;
+            break;
+        }
+        n++;
+    }
+
+    if(flag) cout << n;
+    else cout << -1;
+
+    return 0;
+}
