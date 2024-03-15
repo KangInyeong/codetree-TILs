ed9207ced797d399467949bfa815b5c31b0c2573
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-03-15 16:32:39 +0900

    Title: 배열의 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240315/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[2][4] = {0, };
+    for(int i=0; i<2; i++){
+        for(int j=0; j<4; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << fixed;
+    cout.precision(1);
+
+    int totalsum = 0;
+    for(int i=0; i<2; i++){
+        int sum = 0;
+        for(int j=0; j<4; j++) {
+            sum += arr[i][j];
+        }
+        totalsum += sum;
+        cout << (float)(sum/4) << ' ';
+    }cout << endl;
+
+    for(int i=0; i<4; i++){
+        int sum = 0;
+        for(int j=0; j<2; j++){
+            sum += arr[j][i];
+        }
+        totalsum += sum;
+        cout << (float)(sum/2) << ' ';
+    } cout << endl;
+
+    cout << (float)(totalsum/16);
+
+    return 0;
+}
