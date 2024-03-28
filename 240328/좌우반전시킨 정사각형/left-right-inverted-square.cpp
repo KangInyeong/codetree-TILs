47ad9ebdb9cbfd37b09c0189bf37f4fd80bb2d7e
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-03-28 23:59:15 +0900

    Title: 좌우반전시킨 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240328/좌우반전시킨 정사각형/left-right-inverted-square.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[10][10] = {0, };
+    int n;
+    cin >> n;
+
+    int a = 1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            arr[i][j] = (j+1) * (i+1);
+            a++;
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=n-1; j>=0; j--){
+            cout << arr[i][j] << ' ';
+        }cout << endl;
+    }
+
+    return 0;
+}
