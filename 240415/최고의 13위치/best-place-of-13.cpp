7bf4ae58eb377718d91d42b2571a3648a4964f29
Kang Inyeong <52899340+KangInyeong@users.noreply.github.com>
2024-04-15 23:59:15 +0900

    Title: 최고의 13위치, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240415/최고의 13위치/best-place-of-13.cpp
+++ 240415/최고의 13위치/best-place-of-13.cpp
@@ -14,12 +14,12 @@ int main() {
 
     int cnt = 0;
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j+=3){
+        for(int j=0; j<n-2; j+=2){
             if(arr[i][j]+arr[i][j+1]+arr[i][j+2] > cnt) cnt = arr[i][j]+arr[i][j+1]+arr[i][j+2];
         }
     }
 
     cout << cnt;
-    
+
     return 0;
 }
